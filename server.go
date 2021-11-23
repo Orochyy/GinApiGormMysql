4ab132b045f51a5523d63c83b25b1e11ee5d10c1
admin <yura333619@gmail.com>
2021-11-23 15:49:00 +0200

    work!

---
+++ server.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/ydhnwb/golang_api/config"
+	"github.com/ydhnwb/golang_api/controller"
+	"github.com/ydhnwb/golang_api/middleware"
+	"github.com/ydhnwb/golang_api/repository"
+	"github.com/ydhnwb/golang_api/service"
+	"gorm.io/gorm"
+	"time"
+)
+
+var (
+	db             *gorm.DB                  = config.SetupDatabaseConnection()
+	userRepository repository.UserRepository = repository.NewUserRepository(db)
+	bookRepository repository.BookRepository = repository.NewBookRepository(db)
+	jwtService     service.JWTService        = service.NewJWTService()
+	userService    service.UserService       = service.NewUserService(userRepository)
+	bookService    service.BookService       = service.NewBookService(bookRepository)
+	authService    service.AuthService       = service.NewAuthService(userRepository)
+	authController controller.AuthController = controller.NewAuthController(authService, jwtService)
+	userController controller.UserController = controller.NewUserController(userService, jwtService)
+	bookController controller.BookController = controller.NewBookController(bookService, jwtService)
+)
+
+func main() {
+	defer config.CloseDatabaseConnection(db)
+	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:8081"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	authRoutes := r.Group("api/auth")
+	{
+		authRoutes.POST("/login", authController.Login)
+		authRoutes.POST("/register", authController.Register)
+	}
+
+	userRoutes := r.Group("api/user", middleware.AuthorizeJWT(jwtService))
+	{
+		userRoutes.GET("/profile", userController.Profile)
+		userRoutes.PUT("/profile", userController.Update)
+	}
+
+	bookRoutes := r.Group("api/books", middleware.AuthorizeJWT(jwtService))
+	{
+		bookRoutes.GET("/", bookController.All)
+		bookRoutes.POST("/", bookController.Insert)
+		bookRoutes.GET("/:id", bookController.FindByID)
+		bookRoutes.PUT("/:id", bookController.Update)
+		bookRoutes.DELETE("/:id", bookController.Delete)
+	}
+
+	r.Run("192.168.1.18:8888")
+}
